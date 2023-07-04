21a0f193a3e801efd5cd49d258fce45fa3ab6c1d
walidmadad <77643737+walidmadad@users.noreply.github.com>
2023-07-04 14:53:56 +0200

    Update inscription.php

--- inscription.php
+++ inscription.php
@@ -4,17 +4,17 @@ function hashFunction($data){
     return $hash;
 }
 if(isset($_POST['btn_submit'])){
-    $host = "localhost";
-    $user = "root";
-    $pass="";
-    $db="Conexion_insciption";
+    $host = "localhost";//host
+    $user = "root";//user
+    $pass="";//password
+    $db="Conexion_insciption";// nom de base de données
 
     $prenom= $_POST['prenom'];
     $nom= $_POST['nom'];
     $email= $_POST['email'];
     $password = $_POST['password2'];
-    $password = hashFunction($password);
-    $conn = mysqli_connect($host,$user,$pass,$db);
+    $password = hashFunction($password);//en utiliser HashFunction pour ne pas stocker le mot de passe en claire 
+    $conn = mysqli_connect($host,$user,$pass,$db); 
     $insert = "insert into information values('$prenom', '$nom', '$email', '$password')";
     mysqli_query($conn,$insert);
     echo("Félicitations, votre compte a été créé");
